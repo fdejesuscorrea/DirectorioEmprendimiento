4ca6ed8d7add2ad468a424f3bc2f984f211d2d6d
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-03-18 16:25:35 -0500

    Realizada la configuracion inicial del proyecto

---
+++ src/main/java/com/udea/edu/co/directorio/DirectorioApplication.java
@@ -0,0 +1,13 @@
+package com.udea.edu.co.directorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/edu/co/directorio/DirectorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.edu.co.directorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
