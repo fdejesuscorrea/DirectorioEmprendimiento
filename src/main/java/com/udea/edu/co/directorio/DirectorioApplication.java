de5834ab8eaec334d30e409fcae9f440005ea220
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-03-18 17:35:01 -0500

    Adicionada conexion a base de datos y clases de prueba

--- src/main/java/com/udea/edu/co/directorio/DirectorioApplication.java
+++ src/main/java/com/udea/edu/co/directorio/DirectorioApplication.java
@@ -2,12 +2,19 @@ package com.udea.edu.co.directorio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DirectorioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DirectorioApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Bienvenido al directorio de Emprendimiento";
+	}
 }
