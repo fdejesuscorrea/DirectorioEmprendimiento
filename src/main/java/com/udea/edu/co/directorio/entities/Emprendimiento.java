ddfb163d873eca0ae7fbc73bc5b8bf5ea67bc253
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-04-17 03:59:45 -0500

    Cambios

--- src/main/java/com/udea/edu/co/directorio/entities/Emprendimiento.java
+++ src/main/java/com/udea/edu/co/directorio/entities/Emprendimiento.java
@@ -1,114 +1,41 @@
 package com.udea.edu.co.directorio.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "emprendimientos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Emprendimiento {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(name = "name")
-    private String name;
+    private String nombre;
 
-    @Column(name = "brief_description")
-    private String briefDescription;
-
-    @Column(name = "if_formalized")
-    private Character ifFormalized;
-
-    @Column(name = "email")
     private String email;
 
-    @Column(name = "whatsapp")
+    private String resumen;
+
+
+    private Boolean ifFormalized;
+
+
+
     private String whatsapp;
 
-    @Column(name = "webpage")
     private String webpage;
 
-    @Column(name = "logo_name")
     private String logoName;
 
 
-    @Column(name="super_tipo")
     private String superTipo;
 
     @Column(name="estado_emprendimiento")
     private String estadoEmprendimiento;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getBriefDescription() {
-        return briefDescription;
-    }
-
-    public Character getIfFormalized() {
-        return ifFormalized;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getWhatsapp() {
-        return whatsapp;
-    }
-
-    public String getWebpage() {
-        return webpage;
-    }
-
-    public String getLogoName() {
-        return logoName;
-    }
-
-    public String getSuperTipo() {
-        return superTipo;
-    }
-
-    public String getEstadoEmprendimiento() {
-        return estadoEmprendimiento;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setBriefDescription(String briefDescription) {
-        this.briefDescription = briefDescription;
-    }
-
-    public void setIfFormalized(Character ifFormalized) {
-        this.ifFormalized = ifFormalized;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setWhatsapp(String whatsapp) {
-        this.whatsapp = whatsapp;
-    }
-
-    public void setWebpage(String webpage) {
-        this.webpage = webpage;
-    }
-
-    public void setLogoName(String logoName) {
-        this.logoName = logoName;
-    }
-
-    public void setSuperTipo(String superTipo) {
-        this.superTipo = superTipo;
-    }
-
-    public void setEstadoEmprendimiento(String estadoEmprendimiento) {
-        this.estadoEmprendimiento = estadoEmprendimiento;
-    }
     // Getters and setters
 }
---
+++ src/main/java/com/udea/edu/co/directorio/entities/InformacionDeContacto.java
@@ -0,0 +1,21 @@
+package com.udea.edu.co.directorio.entities;
+
+import lombok.Data;
+
+@Data
+public class InformacionDeContacto {
+
+    private String email;
+
+    private String mobile;
+
+    private String twitter;
+
+    private String instagram;
+
+    private String whatsapp;
+
+    private String linkedin;
+
+    private String webpage;
+}
